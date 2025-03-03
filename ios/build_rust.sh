053540162f364d7470f7e108ac021e6929e450ef
Luis Finke <luisfinke@gmail.com>
2025-03-03 18:09:06 -0500

    just use a shell script for ios building

--- ios/build_rust.sh
+++ ios/build_rust.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -z "$(which make)" ]; then
 	>&2 echo "Missing make command"
@@ -10,5 +10,13 @@ if [ -z "$(which cargo)" ]; then
 	exit 1
 fi
 
+LIB_FILE="libreact_native_librespot.a"
+
 cd "$(dirname "$0")/rust" || exit $?
-make || exit $?
+cargo build
+	--target "x86_64-apple-ios" \
+	--target "aarch64-apple-ios" \
+	--target "aarch64-apple-ios-sim" \
+	--release || exit $?
+mkdir -p lib || exit $?
+lipo -create "target/x86_64-apple-ios/release/$LIB_FILE" "target/aarch64-apple-ios-sim/release/$LIB_FILE" -output "lib/$LIB_FILE" || exit $?
